7a8a226bfa784fc5a5d5ee735212e13f1f782703
Rohit D <rohit.dafda@propelius.tech>
2023-06-20 15:04:18 +0530

    add filters in auth.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ResponseInterceptor } from './interceptor/response.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionFilter } from 'src/filters/all-exception.filter';
+import { NormalExceptionFilter } from 'src/filters/normal-exception.filter';
+import { ValidationExceptionFilter } from 'src/filters/validator-exception.filter';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [
     AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: NormalExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ValidationExceptionFilter,
+    },
     { provide: APP_INTERCEPTOR, useClass: ResponseInterceptor }
   ],
 })
