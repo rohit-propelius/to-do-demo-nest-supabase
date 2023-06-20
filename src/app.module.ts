1e4b7fd0a2c26b7dfc7b7a4b828814d3195afee8
Rohit D <rohit.dafda@propelius.tech>
2023-06-20 17:51:46 +0530

    integrate supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AllExceptionFilter } from 'src/filters/all-exception.filter';
 import { NormalExceptionFilter } from 'src/filters/normal-exception.filter';
 import { ValidationExceptionFilter } from 'src/filters/validator-exception.filter';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
 
 @Module({
   imports: [],
@@ -24,7 +25,14 @@ import { ValidationExceptionFilter } from 'src/filters/validator-exception.filte
       provide: APP_FILTER,
       useClass: ValidationExceptionFilter,
     },
-    { provide: APP_INTERCEPTOR, useClass: ResponseInterceptor }
+    { provide: APP_INTERCEPTOR, 
+      useClass: ResponseInterceptor 
+    },
+    {
+      provide: 'SUPABASE_CLIENT',
+      useFactory: (): SupabaseClient =>
+        createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY),
+    }
   ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { SupabaseClient } from '@supabase/supabase-js';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World! Welcome to To Do demo.';
+  constructor(
+    @Inject('SUPABASE_CLIENT')
+    private readonly supabaseClient: SupabaseClient
+  ){}
+  async getHello() {
+    const user = await this.supabaseClient.from('users').select('*');
+
+    return {
+      user,
+      message:'Hello World! Welcome to To Do demo.'
+    };
   }
 }
